20b96020b2f7d88b4c6c64da5e236843a7d34fc3
nsirtak <nsirtak@gmail.com>
2025-09-07 17:08:44 -0500

    first commit

---
+++ 01_arrays/arrays.js
@@ -0,0 +1,19 @@
+const strings = ["a", "b", "c", "d"];
+const numbers = [1, 2, 3, 4, 5];
+// Variable array is somewhere in memory and the computer knows it.
+// When I do array[2], i'm telling the computer, hey go to the array and grab the 3rd item from where the array is stored.
+
+//push
+strings.push("e"); //Adds 'e' to the end of the array. outcome: ["a", "b", "c", "d", "e"]
+
+//pop
+strings.pop(); //Removes the last item from the array. outcome: ["a", "b", "c", "d"]. outcome: ['a', 'b', 'c', 'd']
+strings.pop(); //outcome: ['a', 'b', 'c']
+
+//unshift
+strings.unshift("x"); //Adds 'x' to the front of the array. outcome: ['x', 'a', 'b', 'c']
+
+//splice
+strings.splice(2, 0, "alien"); //At index 2, remove 0 items and add 'alien'. So index 2 is right before 'b'. outcome: ['x', 'a', 'alien', 'b', 'c']
+
+console.log(strings); //Final outcome: ['x', 'a', 'alien', 'b', 'c']
